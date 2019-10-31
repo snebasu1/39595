1b2332bb3e58ee61059e3b4f3f7af0c7a34f2569
Abhishek Vyas <vyas6@purdue.edu>
2019-10-31 17:03:26 -0400

    stuff

--- project/ByteCode.java
+++ project/ByteCode.java
@@ -55,40 +55,184 @@ public class ByteCode{
   public void execute(){
     switch(this.command){
       case "cmpe":
+        cmpe obj = new cmpe();
+        obj.execute();
+        break;
+
       case "cmplt":
+        cmplt obj = new cmplt();
+        obj.execute();
+        break;
+
       case "cmpgt":
+        cmpgt obj = new cmpgt();
+        obj.execute();
+        break;
+
       case "jmp":
+        jmp obj = new jmp();
+        obj.execute();
+        break;
+
       case "jmpc":
+        jmpc obj = new jmpc();
+        obj.execute();
+        break;
+
       case "call":
+        call obj = new call();
+        obj.execute();
+        break;
+
       case "ret":
+        ret obj = new ret();
+        obj.execute();
+        break;
+
       case "pushc":
+        pushc obj = new pushc();
+        obj.execute();
+        break;
+
       case "pushs":
+        pushs obj = new pushs();
+        obj.execute();
+        break;
+
       case "pushi":
+        pushi obj = new pushi();
+        obj.execute();
+        break;
+
       case "pushf":
+        pushf obj = new pushf();
+        obj.execute();
+        break;
+
       case "pushvc":
+        pushvc obj = new pushvc();
+        obj.execute();
+        break;
+
       case "pushvs":
+        pushvs obj = new pushvs();
+        obj.execute();
+        break;
+
       case "pushvi":
+        pushvi obj = new pushvi();
+        obj.execute();
+        break;
+
       case "pushvf":
+        pushvf obj = new pushvf();
+        obj.execute();
+        break;
+
       case "popm":
+        popm obj = new popm();
+        obj.execute();
+        break;
+
       case "popv":
+        popv obj = new popv();
+        obj.execute();
+        break;
+
       case "popa":
+        popa obj = new popa();
+        obj.execute();
+        break;
+
       case "peekc":
+        peekc obj = new peekc();
+        obj.execute();
+        break;
+
       case "peeks":
+        peeks obj = new peeks();
+        obj.execute();
+        break;
+
       case "peeki":
+        peeki obj = new peeki();
+        obj.execute();
+        break;
+
       case "peekf":
+        peekf obj = new peekf();
+        obj.execute();
+        break;
+
       case "pokec":
+        pokec obj = new pokec();
+        obj.execute();
+        break;
+
       case "pokes":
+        pokes obj = new pokes();
+        obj.execute();
+        break;
+
       case "pokei":
+        pokei obj = new pokei();
+        obj.execute();
+        break;
+
       case "pokef":
+        pokef obj = new pokef();
+        obj.execute();
+        break;
+
       case "swp":
+        swp obj = new swp();
+        obj.execute();
+        break;
+
       case "add":
+        add obj = new add();
+        obj.execute();
+        break;
+
       case "sub":
+        sub obj = new sub();
+        obj.execute();
+        break;
+
       case "mul":
+        mul obj = new mul();
+        obj.execute();
+        break;
+
       case "div":
+        div obj = new div();
+        obj.execute();
+        break;
+
       case "printc":
+        printc obj = new printc();
+        obj.execute();
+        break;
+
       case "printi":
+        printi obj = new printi();
+        obj.execute();
+        break;
+
       case "printf":
+        printf obj = new printf();
+        obj.execute();
+        break;
+
       case "prints":
+        prints obj = new prints();
+        obj.execute();
+        break;
+
+      case "halt":
+        halt obj = new halt();
+        obj.execute();
+        break;
     }
   }
 
